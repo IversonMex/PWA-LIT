1092d57a9d20f6e39898497f4b2364ead9396234
Iverson G.M <iverson.19070044@itsmotul.edu.mx>
2023-02-09 11:58:12 -0600

    Paso 3

--- Working with lists/my-element.ts
+++ Working with lists/my-element.ts
@@ -1,17 +1,18 @@
 import {LitElement, html} from 'lit';
 import {customElement, state} from 'lit/decorators.js';
-import {map} from 'lit/directives/map.js';
 
 @customElement('my-element')
 class MyElement extends LitElement {
   @state()
-  items = new Set(['Manzana', 'Platano', 'Fresa', 'Naranja', 'Limón'])
+  names = ['Adrian', 'Emilio', 'Iverson', 'Sebastian', 'Rachel', 'Ross'];
 
   render() {
     return html`
-      <p>Frutas</p>
+      <p>Lista de nombres que incluyen e</p>
       <ul>
-        ${map(this.items, (item) => html`<li>${item}</li>`)}
+      ${this.names
+        .filter((name) => name.match(/e/i))
+        .map((name) => html`<li>${name}</li>`)}
       </ul>
     `;
   }
