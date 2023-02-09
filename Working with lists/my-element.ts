d6a3ea12e639b64531685da11f538aa741d6d8db
Iverson G.M <iverson.19070044@itsmotul.edu.mx>
2023-02-09 12:03:09 -0600

    Paso 4

--- Working with lists/my-element.ts
+++ Working with lists/my-element.ts
@@ -1,19 +1,45 @@
 import {LitElement, html} from 'lit';
 import {customElement, state} from 'lit/decorators.js';
+import type {TemplateResult} from 'lit';
 
 @customElement('my-element')
 class MyElement extends LitElement {
   @state()
-  names = ['Adrian', 'Emilio', 'Iverson', 'Sebastian', 'Rachel', 'Ross'];
+  friends = ['Iverson', 'Yasif', 'Seabastian'];
+
+  @state()
+  pets = [
+    { name: "Hedwig", species: "Owl" },
+    { name: "Scabbers", species: "Rat" },
+    { name: "Crookshanks", species: "Cat" },
+  ];
+
+  @state()
+  includePets = true;
 
   render() {
+    const listItems: TemplateResult[] = [];
+    this.friends.forEach((friend) => {
+      listItems.push(html`<li>${friend}</li>`);
+    });
+    if (this.includePets) {
+      this.pets.forEach((pet) => {
+        listItems.push(html`<li>${pet.name} (${pet.species})</li>`);
+      });
+    }
+
     return html`
-      <p>Lista de nombres que incluyen e</p>
+      <button @click=${() => this._togglePetVisibility()}>
+        ${this.includePets ? 'ver' : 'Ocultar'} sus mascotas
+      </button>
+      <p>Lista de amigos</p>
       <ul>
-      ${this.names
-        .filter((name) => name.match(/e/i))
-        .map((name) => html`<li>${name}</li>`)}
+        ${listItems}
       </ul>
     `;
+  }
+
+  private _togglePetVisibility() {
+    this.includePets = !this.includePets;
   }
 }
