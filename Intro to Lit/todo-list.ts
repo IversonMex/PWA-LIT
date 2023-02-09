e521cda18064a7000c0c9ded3a33699d284a03c2
Iverson G.M <iverson.19070044@itsmotul.edu.mx>
2023-02-09 11:44:15 -0600

    Paso 7

--- Intro to Lit/todo-list.ts
+++ Intro to Lit/todo-list.ts
@@ -1,12 +1,24 @@
-import {LitElement, html} from 'lit';
+import {LitElement, html, css} from 'lit';
 import {customElement, state, query} from 'lit/decorators.js';
+
+type ToDoItem = {
+  text: string,
+  completed: boolean
+}
 
 @customElement('todo-list')
 export class ToDoList extends LitElement {
+  static styles = css`
+    .completed {
+      text-decoration-line: line-through;
+      color: #777;
+    }
+  `;
+
   @state()
   private _listItems = [
     { text: 'Nombre', completed: true },
-    { text: 'Apellido', completed: false }
+    { text: 'Apellido', completed: true }
   ];
 
   render() {
@@ -14,11 +26,22 @@ export class ToDoList extends LitElement {
       <h2>Lista</h2>
       <ul>
         ${this._listItems.map((item) =>
-          html`<li>${item.text}</li>`)}
+          html`
+            <li
+                class=${item.completed ? 'completed' : ''}
+                @click=${() => this.toggleCompleted(item)}>
+              ${item.text}
+            </li>`
+        )}
       </ul>
       <input id="newitem" aria-label="New item">
       <button @click=${this.addToDo}>Enviar</button>
     `;
+  }
+
+  toggleCompleted(item: ToDoItem) {
+    item.completed = !item.completed;
+    this.requestUpdate();
   }
 
   @query('#newitem')
